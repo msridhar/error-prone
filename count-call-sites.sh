9ef5553ebc0e315a3dc776ba71c6062efda319ac
Manu Sridharan <msridhar@gmail.com>
2020-01-25 09:55:36 -0800

    script to count verified call sites

---
+++ count-call-sites.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# note: requires ripgrep to have already been installed: https://github.com/BurntSushi/ripgrep
+
+if [ -e showChecksOutput.txt.gz ]
+then 
+  gzip -d showChecksOutput.txt.gz
+fi
+if [ ! -e showChecksOutput.txt ]
+then
+  echo "saved output missing; aborting"
+  exit 1
+fi
+COUNT=$(rg -U --count "build\(\)\n(.*)\n   expected: DECLARED @CalledMethods\(.*\n success" showChecksOutput.txt)
+echo "${COUNT} verified calls"
